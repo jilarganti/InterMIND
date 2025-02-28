1cef6eeb78a6907f58956d31fb414dd236031bb9
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-02-28 17:18:07 +0400

    refactor: remove deprecated server code and update type definitions for VitePress configuration

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import bodyParser from "body-parser"
-import handler from "./api/createContactAndLead.js"
-
-const app = express()
-app.use(bodyParser.json())
-app.post("/api/createContactAndLead", handler)
-app.listen(3000)
