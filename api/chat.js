de3597451b009de54b0992f690eae616d0e85bd0
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-03-03 12:58:41 +0400

    refactor: update chat handler to support streaming responses and improve message processing

--- api/chat.js
+++ api/chat.js
@@ -1,21 +1,20 @@
 import { openai } from "@ai-sdk/openai"
 import { anthropic } from "@ai-sdk/anthropic"
 import { streamText } from "ai"
-import dotenv from "dotenv"
 
-dotenv.config()
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30
 
-/**
- * @param {{ body: { messages: any; }; }} req
- */
-export default async function chatHandler(req) {
-  const { messages } = req.body
-  const response = streamText({
+// @ts-ignore
+export async function POST(req) {
+  const { messages } = await req.json()
+
+  const result = streamText({
+    // model: openai("gpt-4-turbo"),
     model: anthropic("claude-3-haiku-20240307"),
+    system: "You are a helpful assistant.",
     messages,
   })
 
-  return response.toDataStreamResponse()
+  return result.toDataStreamResponse()
 }
-
-// TODO https://vercel.com/docs/functions/streaming-functions
