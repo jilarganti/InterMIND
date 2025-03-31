de2814fa4292a2d4612ce1a0e80d9c5de6eb906f
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-03-31 14:31:43 +0400

    refactor: clean up vercel.json by removing unused routes and enabling clean URLs

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -31,8 +31,6 @@ export default {
         const cookieLang = document.cookie.match(/nf_lang=([^;]+)/)?.[1]?.slice(0, 2)
         const path = page.value.relativePath.replace(PATH_CLEANUP_REGEX, "")
 
-        // console.log(browserLang, currentLang, cookieLang, path)
-
         if (!cookieLang) {
           document.cookie = `nf_lang=${browserLang}; expires=Mon, 1 Jan 2030 00:00:00 UTC; path=/`
           if (currentLang !== browserLang) {
