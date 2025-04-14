cfde9669ce2e413a71f4aa080d5782c9ead0c846
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-04-14 13:15:53 +0400

    refactor: enhance layout with authentication buttons and update component exports

--- packages/golden-fish/docs/.vitepress/theme/index.ts
+++ packages/golden-fish/docs/.vitepress/theme/index.ts
@@ -1,14 +1,31 @@
 import { h } from "vue"
 import DefaultTheme from "vitepress/theme"
 import "./style.css"
-import { useData, inBrowser, useRouter } from "vitepress"
 
 import { components } from "shared"
 import sharedTheme from "shared"
 
+const { ContactFormModal, NavButton } = components
+
 // Просто расширяем тему из shared и регистрируем те же компоненты
 export default {
   ...sharedTheme,
+  Layout() {
+    // const { frontmatter } = useData()
+
+    return h(DefaultTheme.Layout, null, {
+      "nav-bar-content-after": () =>
+        h("div", { class: "auth-buttons-container" }, [
+          h(ContactFormModal, {
+            buttonText: "Send request",
+            // formStyle: "display: block; margin: 20px auto;",
+          }),
+          h(NavButton, { text: "Ask AI", buttonClass: "alt", to: "/chat" }),
+        ]),
+      // "aside-ads-before": () => h(AsideSponsors),
+      // "home-features-after": () => h(HomeSponsors),
+    })
+  },
   // Если нужно переопределить какие-то методы темы, можно это сделать здесь
   // enhanceApp({ app, router }) {},
 }
