1861c34635aec8beb033381485a21286cd6cb2a6
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-05-06 11:20:44 +0400

    fix: refactor computed properties in HeroSection for clarity and consistency

--- packages/i14n/docs/.vitepress/components/HeroSection.vue
+++ packages/i14n/docs/.vitepress/components/HeroSection.vue
@@ -2,7 +2,7 @@
 import { computed } from "vue"
 // Import VPButton - Adjust path if necessary based on your VitePress setup
 import VPButton from "vitepress/dist/client/theme-default/components/VPButton.vue"
-import { renderMarkdown } from "../../../../../shared/utils/markdown"
+// import { renderMarkdown } from "../../../../../shared/utils/markdown"
 
 interface ActionItem {
   text: string
@@ -17,22 +17,19 @@ const props = defineProps({
   actions: { type: Array as () => ActionItem[], default: () => [] },
 })
 
+// Regex to find text enclosed in double asterisks
+const regex = /\*\*(.*?)\*\*/
+
 // Computed property to style the word(s) enclosed in ** **
-const styledTitle = computed(() => {
-  // Regex to find text enclosed in double asterisks
-  const regex = /\*\*(.*?)\*\*/
-  // Replace the pattern with the span tag
-  const highlightedTitle = props.title.replace(regex, '<span class="highlighted-word">$1</span>')
-  return highlightedTitle
-})
-const renderedText = computed(() => renderMarkdown(props.text))
+const styledTitle = computed(() => props.title.replace(regex, '<span class="highlighted-word">$1</span>'))
+const styledText = computed(() => props.text.replace(regex, '<span class="hl">$1</span>'))
 </script>
 
 <template>
   <section class="hero-section">
     <!-- Use v-html to render the styled title -->
     <h1 class="hero-title" v-html="styledTitle"></h1>
-    <p class="hero-text" v-html="renderedText"></p>
+    <p class="hero-text" v-html="styledText"></p>
     <div v-if="actions && actions.length" class="hero-actions">
       <!-- Use VPButton component -->
       <VPButton v-for="(action, idx) in actions" :key="idx" :text="action.text" :href="action.link" :theme="action.theme" />
@@ -97,7 +94,6 @@ const renderedText = computed(() => renderMarkdown(props.text))
   }
 
   .hero-actions {
-    /* Add this rule for hero-actions */
     margin-top: 2.5rem; /* Use fixed margin for smaller screens */
   }
 }
