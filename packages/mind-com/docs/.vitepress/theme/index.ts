4b0933bd34ac0676242be74a8e354615a9eba6c7
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-05-23 11:37:24 +0400

    refactor: Update project structure and configuration, renaming i14n to mind-com and adjusting paths in jsconfig.json

--- packages/mind-com/docs/.vitepress/theme/index.ts
+++ packages/mind-com/docs/.vitepress/theme/index.ts
@@ -1,22 +1,19 @@
 import { h } from "vue"
 import { type App } from "vue"
 import DefaultTheme from "vitepress/theme"
-// import { useData } from "vitepress"
 import "./style.css"
 
 import { components } from "shared"
 import sharedTheme from "shared"
 import AuthButton from "../components/AuthButton.vue"
-// import HeroSection from "../components/HeroSection.vue"
 
-const { ContactFormModalNav, NavButton, SearchInput, FeatureBlock } = components
+const { SearchInput } = components
 
 // Расширяем тему из shared и регистрируем компоненты
 export default {
   ...sharedTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      // Для десктопов (в навбаре)
       "nav-bar-content-before": () => {
         return h(SearchInput)
       },
@@ -25,11 +22,6 @@ export default {
           h(AuthButton, { text: "Sign in", buttonClass: "alt" }),
           // h(NavButton, { text: "Ask AI", buttonClass: "alt", to: "/chat" }),
         ]),
-      // "home-hero-actions-after": () =>
-      //   h("div", { class: "auth-buttons-container" }, [
-      //     h(AuthButton, { text: "Sign in", buttonClass: "brand" }),
-      //     h(NavButton, { text: "Ask AI", buttonClass: "alt", to: "/chat" }),
-      //   ]),
     })
   },
   enhanceApp({ app, router }: { app: App; router: any }) {
@@ -39,8 +31,6 @@ export default {
       sharedTheme.enhanceApp({ app, router })
     }
 
-    // i14n-specific component registrations
-    // app.component("HeroSection", HeroSection)
     app.component("AuthButton", AuthButton)
   },
 }
