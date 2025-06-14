9cae0fd6ef0e69e4b352009776797ea9b3102562
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-06-14 16:07:23 +0400

    fix: remove unused ContactFormModal imports from components index

--- shared/components/index.ts
+++ shared/components/index.ts
@@ -4,8 +4,6 @@ import LogoGrid from "./LogoGrid.vue"
 import Button from "./Button.vue"
 import FeatureCards from "./FeatureCards.vue"
 import BenefitsList from "./BenefitsList.vue"
-import ContactFormModal from "./CRM/ContactFormModal.vue"
-import ContactFormModalNav from "./CRM/ContactFormModalNav.vue"
 import AIChat from "./AIChat/AIChat.vue"
 import FeatureBlock from "./FeatureBlock.vue"
 import NavButton from "./NavButton.vue"
@@ -28,8 +26,6 @@ export const components: Components = {
   FeatureCards,
   BenefitsList,
   AIChat,
-  // ContactFormModal,
-  // ContactFormModalNav,
   FeatureBlock,
   NavButton,
   SearchInput,
