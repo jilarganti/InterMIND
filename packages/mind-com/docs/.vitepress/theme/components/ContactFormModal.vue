e14ea9a8c07181c241941d8bd1394e5f897d6fc2
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2025-06-16 12:02:07 +0400

    chore: fix

--- packages/mind-com/docs/.vitepress/theme/components/ContactFormModal.vue
+++ packages/mind-com/docs/.vitepress/theme/components/ContactFormModal.vue
@@ -29,8 +29,8 @@ import { ref, computed, onMounted } from "vue"
 import { onClickOutside } from "@vueuse/core"
 import VPButton from "vitepress/dist/client/theme-default/components/VPButton.vue"
 import { useFormSubmit } from "../composables/useFormSubmit"
-import { generateOriginId } from "../../../../../shared/utils/path"
-import { determineTrafficSource, initUtmTracking } from "../../../../../shared/utils/utm"
+import { generateOriginId } from "../../../../../../shared/utils/path"
+import { determineTrafficSource, initUtmTracking } from "../../../../../../shared/utils/utm"
 
 const { site, page } = useData()
 
